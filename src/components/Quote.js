32649d75a68f518b2a7e05aff0a855668d616e2b
Meghna Srivastava <meghna.s1906@gmail.com>
2018-08-09 14:47:04 +0200

    changed api

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,49 +1,61 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Quote extends Component {
-    state = {
-        quote: "",
-        author: ""
-    }
+  state = {
+    quote: "",
+    author: ""
+  };
 
-    componentDidMount() {
-        this.getQuote()
-    }
+  componentDidMount() {
+    this.getQuote();
+  }
 
-    getQuote = () => {
+  getQuote = () => {
+    axios
+      .get("https://talaikis.com/api/quotes/random/")
+      .then(response => {
+        console.log(response.data);
+        let quote, author;
+        quote = response.data.quote;
+        author = response.data.author;
 
-        axios.get('https://random-quote-generator.herokuapp.com/api/quotes/random')
-            .then(response => {
-                console.log(response.data);
-                let quote, author;
-                quote = response.data.quote
-                author = response.data.author
-                this.setState({ quote, author })
-            })
-            .catch(err => {
-                console.log("error", err)
-            })
-    }
+        this.setState({ quote, author });
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
 
-    tweetQuote = () => {
-        console.log('tweet', this.state)
-        let tweetUrl = `https://twitter.com/intent/tweet?text="${this.state.quote}"-${this.state.author}`
-        window.open(tweetUrl, "", "width=500,height=300");
-    }
+  tweetQuote = () => {
+    console.log("tweet", this.state);
+    let tweetUrl = `https://twitter.com/intent/tweet?text="${
+      this.state.quote
+    }"-${this.state.author}`;
+    window.open(tweetUrl, "", "width=500,height=300");
+  };
 
-    render() {
-        return (
-            <div className="container">
-                <div className="quote-card">
-                    <h2><strong>{this.state.quote}</strong></h2>
-                    <h4><i>&mdash; {this.state.author}</i></h4>
-                </div>
-                <button className="quote" onClick={this.getQuote}>Get Quote</button>
-                <button className="fa fa-twitter" onClick={this.tweetQuote}> Tweet</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="quote-card">
+          <h2>
+            <strong>{this.state.quote}</strong>
+          </h2>
+          <h4>
+            <i>&mdash; {this.state.author}</i>
+          </h4>
+        </div>
+        <button className="quote" onClick={this.getQuote}>
+          Get Quote
+        </button>
+        <button className="fa fa-twitter" onClick={this.tweetQuote}>
+          {" "}
+          Tweet
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Quote;
